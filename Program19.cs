8192d86a08af8dfda58648e3789a0e354f310be0
Rahultyagi2345 <162874915+Rahultyagi2345@users.noreply.github.com>
2025-02-28 12:51:02 +0530

    Update Program19.cs

---
+++ Program19.cs
@@ -0,0 +1,26 @@
+
+using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        string inputFile = "meters.txt";
+        string outputFile = "feet.txt";
+
+        // Reading from meters.txt
+        string[] lines = File.ReadAllLines(inputFile);
+        using (StreamWriter writer = new StreamWriter(outputFile))
+        {
+            foreach (string line in lines)
+            {
+                double meters = double.Parse(line);
+                double feet = meters * 3.28084;
+                writer.WriteLine($"{meters} meters = {feet} feet");
+            }
+        }
+    }
+}
+Program19.cs
+Displaying Program19.cs.
